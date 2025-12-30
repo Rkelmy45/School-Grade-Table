cf96f8441a3696279c4d7fdc1c2bb86b1eb22575
Rikelmi Peña <rikelmitejeda@gmail.com>
2025-12-30 14:49:27 -0500

    React: Creation of frontend

---
+++ frontend/src/Components/StudentScore.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function StudentScore () {
+    return (
+
+    );
+}
+
+export default StudentScore;
---
+++ frontend/src/Components/UserLogin.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function UserLogin () {
+    return (
+
+    );
+}
+
+export default UserLogin;
